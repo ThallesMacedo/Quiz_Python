aef248e7084262d47941c3145ecdddbb3105fef0
Thalles <thallesemail@mail.com>
2026-02-05 20:56:56 -0300

    Criando as perguntas do meu quiz e suas condições de acerto ou não

--- quiz.py
+++ quiz.py
@@ -9,3 +9,48 @@ if resposta_usuario != "SIM":
     quit()
 
 print("Começando...")
+
+#1º Pergunta
+print("Para que serve o Git? \n (A) Editar imagens \n (B) Controlar versões de código \n (C) Criar jogos \n")
+r1 = input("Resposta: ")
+
+if r1 == "A":
+    print("Correto!")
+else:
+    print("Incorreto!")
+
+#2º Pergunta
+print("Qual comando inicia um repositório Git? \n (A) git start \n (B) git init \n (C) git create \n")
+r2 = input("Resposta: ")
+
+if r2 == "B":
+    print("Correto!")
+else:
+    print("Incorreto!")
+
+#3º Pergunta
+print("Qual comando mostra o estado dos arquivos? \n (A) git log \n (B) git status \n (C) git check \n")
+r3 = input("Resposta: ")
+
+if r3 == "B":
+    print("Correto!")
+else:
+    print("Incorreto!")
+
+#4º Pergunta
+print("Qual comando adiciona arquivos para o commit? \n (A) git add \n (B) git push \n (C) git pull \n")
+r4 = input("Resposta: ")
+
+if r4 == "A":
+    print("Correto!")
+else:
+    print("Incorreto!")
+
+#5º Pergunta
+print("Git e GitHub são a mesma coisa? \n (A) Sim \n (B) Não \n (C) Às vezes \n")
+r5 = input("Resposta: ")
+
+if r5 == "B":
+    print("Correto!")
+else:
+    print("Incorreto!")
