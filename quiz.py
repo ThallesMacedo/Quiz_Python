70dc005ed74b15c75c13c36f85bdc2a43dcfa641
Thalles <thallesemail@mail.com>
2026-02-05 20:41:03 -0300

    Criando condição para o usuario escolher se quer começar o quiz ou não

--- quiz.py
+++ quiz.py
@@ -1,3 +1,11 @@
 print("Bem vindo ao quiz, meu projeto de desenvolvimento")
-resposta_usuario = input("Quer começar? [S/N]")
+resposta = input("Quer começar? [SIM/NÃO] ") #RECEBENDO A RESPOSTA DO USUARIO
+resposta_usuario = resposta.upper() #TRATANDO A RESPOSTA DEIXANDO A LETRA MAIUSCULA
+
 print(resposta_usuario)
+
+if resposta_usuario != "SIM":
+    print("Encerrando o Quiz, até mais...")
+    quit()
+
+print("Começando...")
